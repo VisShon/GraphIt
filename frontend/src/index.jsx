20b29f7c8691564ca6aeceb8c1bb7f62a21acb79
Vishnu Shon <88622653+VisShon@users.noreply.github.com>
2022-12-06 21:42:55 +0530

    updates

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
-import App from './App';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import Department from './pages/Department';
+import Profile from './pages/Profile';
 
 const client = ApolloClient({
   uri: 'http://localhost:8000/graphql',
@@ -17,7 +20,10 @@ root.render(
     <ApolloProvider client={client}>
       <Navbar/>
       <Routes>
-        <Route path="/home" element={<App/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/department/:id" element={<Department/>}/>
+        <Route path="/profile/:id" element={<Profile/>}/>
+        <Route path="/*" element={<Error/>}/>
       </Routes>
     </ApolloProvider>
   </BrowserRouter>
